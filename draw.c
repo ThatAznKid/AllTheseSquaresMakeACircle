399812a5419d037b6cdf8522aedd323354512e84
Richard <Richard>
2017-02-12 21:58:45 -0500

    stuff

--- draw.c
+++ draw.c
@@ -7,5 +7,23 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+
+  int dy = y1 - y0;
+  int dx = x0 - x1;
+  int D = (2 * A) + B;
+
+  int x = x0;
+  int y = y0;
+  
+  while (x < x1) {
+    plot(s, c, x, y);
+    if (D > 0) {
+      y++;
+      D += 2 * dx;
+    }
+    x++;
+    D += 2 * dy;
+  }
+
 }
 
--- main.c
+++ main.c
@@ -18,6 +18,15 @@ int main() {
   
   clear_screen(s);
 
+  int x = 0;
+	
+  for (int x = 0; x < XRES; x+=10) { 
+    c.red = x/2;
+    c.green = x/3;
+    c.blue = x/4;
+    
+    draw_line(0, 0, x, YRES, s, c);
+  }
 
   display(s);
   save_extension(s, "lines.png");
